846ab66fa14c519814287d57e155e73a909e0f5d
Tymur <batulintimur@gmail.com>
2026-06-03 12:11:34 +0200

    Initial Commit

---
+++ RnbetterSmash/RnbetterSmashApp.swift
@@ -0,0 +1,17 @@
+//
+//  RnbetterSmashApp.swift
+//  RnbetterSmash
+//
+//  Created by Tymur on 03.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct RnbetterSmashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
